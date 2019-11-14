882fabdc1e6d45f9ed37890bfe0fab2477a25aed
distroJack <jswim26@gmail.com>
2019-11-14 02:35:48 -0500

    [piebind] renamed pybind and updated method signratures

--- piebind/src/demo_module.cpp
+++ piebind/src/demo_module.cpp
@@ -5,12 +5,20 @@
 
 static PyObject* HelloWorld(PyObject *self, PyObject *args)
 {
+	
   std::cout << "Hello World" << std::endl;
   Py_RETURN_NONE;
 }
 
+static PyObject *
+ReturnOne(PyObject *self, PyObject *args)
+{
+    return Py_BuildValue("i", 1);
+}
+
 static PyMethodDef demo_methods[] = {
-  {"HelloWorld", (PyCFunction)HelloWorld, METH_VARARGS, "Demo hello world" },
+  {"HelloWorld", (PyCFunction)HelloWorld, METH_NOARGS, "Demo hello world" },
+  {"ReturnOne", (PyCFunction)ReturnOne, METH_NOARGS, "different type" },
   {nullptr, nullptr, 0, nullptr}
 };
 
